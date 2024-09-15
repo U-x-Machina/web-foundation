4cbe8c1c3a9d70abccedfba482b5a594c9d4d4c2
maciej-uxmachina <maciej@uxmachina.co>
2024-09-15 14:39:01 +0200

    fix: terraform config

--- infrastructure/gcp_project.tf
+++ infrastructure/gcp_project.tf
@@ -4,14 +4,14 @@ provider "google" {
 
 resource "random_id" "id" {
   byte_length = 2
-  prefix      = "${replace(lower(var.gcp_project_name), "/\\s+/", "-")}-"
+  prefix      = "uxm-${replace(lower(terraform.workspace), "/\\s+/", "-")}-"
   keepers = {
-    ami_id = var.gcp_project_name
+    ami_id = terraform.workspace
   }
 }
 
 resource "google_project" "project" {
-  name            = var.gcp_project_name
+  name            = "${terraform.project}: ${terraform.workspace}"
   project_id      = random_id.id.hex
   billing_account = var.gcp_billing_account
   org_id          = var.gcp_org_id
@@ -21,12 +21,17 @@ resource "google_project_service" "services" {
   count   = length(var.google_project_services)
   project = google_project.project.project_id
   service = var.google_project_services[count.index]
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 output "gcp_region" {
   value = var.gcp_region
 }
+
+output "gcp_project_name" {
+  value = google_project.project.name
+}
+
 
 output "gcp_project_id" {
   value = google_project.project.project_id
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,15 +14,6 @@ variable "gcp_billing_account" {
   description = "Google Cloud Billing Account ID to be associated with the created project"
 }
 
-variable "gcp_project_name" {
-  type = string
-  description = "Name of the Google Cloud Platform Project to be created."
-  validation {
-    condition     = length(var.gcp_project_name) > 0 && length(var.gcp_project_name) < 26
-    error_message = "Project name needs to be max. 25 characters long. There is a 5-character suffix being added automatically and GCP project names need to be at max. 30 chars long."
-  }
-}
-
 variable "gcp_region" {
   type = string
   description = "Each Cloud Run service or job resides in a region. Customer data associated with the service or job is stored in the selected region. Traffic can be served from multiple regions by configuring external HTTP(S) Load Balancing."
