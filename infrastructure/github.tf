590466806525e9fa3860f4a822706c4f56cd4543
maciej-uxmachina <maciej@uxmachina.co>
2024-09-22 20:17:23 +0200

    fix: try to use full repo name

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -7,7 +7,7 @@ data "github_repository" "repo" {
 # Save MongoDB Atlas connection strings to env vars
 resource "github_actions_environment_variable" "mondogb_connection_strings" {
   for_each      = var.environments
-  repository    = data.github_repository.repo.name
+  repository    = data.github_repository.repo.full_name
   environment   = each.value.name
   variable_name = "MONGODB_CONNECTION_STRING"
   value         = mongodbatlas_serverless_instance.instances[each.key].connection_strings_standard_srv
