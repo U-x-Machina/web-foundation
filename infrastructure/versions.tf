33e498115ec00c4dd23d215f16c1af36ee9b5194
maciej-uxmachina <maciej@uxmachina.co>
2024-09-22 19:33:30 +0200

    fix: add missing git provider

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -10,6 +10,10 @@ terraform {
       source = "mongodb/mongodbatlas"
       version = "1.20.0"
     }
+    git = {
+      source = "metio/git"
+      version = "2024.9.13"
+    }
     github = {
       source  = "integrations/github"
       version = "~> 6.0"
