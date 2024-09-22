07055a9b87dba43f788c96a32132c8f5fa665f36
maciej-uxmachina <maciej@uxmachina.co>
2024-09-22 20:15:08 +0200

    fix: try to reference org repo and env

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -4,17 +4,11 @@ data "github_repository" "repo" {
   full_name = "${var.github_org}/${var.github_repo == "" ? terraform.workspace : var.github_repo}"
 }
 
-resource "github_repository_environment" "environments" {
-  for_each      = var.environments
-  repository    = data.github_repository.repo.name
-  environment   = each.value.name
-}
-
 # Save MongoDB Atlas connection strings to env vars
 resource "github_actions_environment_variable" "mondogb_connection_strings" {
   for_each      = var.environments
   repository    = data.github_repository.repo.name
-  environment   = github_repository_environment.environments[each.key].environment
+  environment   = each.value.name
   variable_name = "MONGODB_CONNECTION_STRING"
   value         = mongodbatlas_serverless_instance.instances[each.key].connection_strings_standard_srv
 }
