6cea68307058ce013fda3a9e1838bd4d96eba71f
maciej-uxmachina <maciej@uxmachina.co>
2024-09-26 14:43:26 +0200

    feat: direct VPS egress attempt

--- infrastructure/gcp_cloud_run.tf
+++ infrastructure/gcp_cloud_run.tf
@@ -39,8 +39,10 @@ resource "google_cloud_run_v2_service" "services" {
       for_each = var.gcp_use_nat_for_mongodb_atlas ? [1] : []
 
       content {
-        connector = google_vpc_access_connector.nat["${each.value.service.name}.${each.value.region}"].id
-        egress    = "ALL_TRAFFIC"
+        network_interfaces {
+          network    = google_compute_network.nat[0].id
+          subnetwork = google_compute_subnetwork.nat[0].id
+        }
       }
     }
     max_instance_request_concurrency = each.value.service.concurrency
--- infrastructure/gcp_nat.tf
+++ infrastructure/gcp_nat.tf
@@ -10,62 +10,48 @@ resource "google_compute_network" "nat" {
 }
 
 resource "google_compute_subnetwork" "nat" {
-  for_each      = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
+  count         = var.gcp_use_nat_for_mongodb_atlas ? 1 : 0
+#   for_each      = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
   provider      = google-beta
   project       = google_project.project.project_id
-  name          = "static-egress-ip-${each.value.service.name}-${each.value.region}"
-  ip_cidr_range = "10.${index(local.subnet_indices, "${each.value.service.name}.${each.value.region}")}.0.0/28"
+  name          = "static-egress-ip"
+#   name          = "static-egress-ip-${each.value.service.name}-${each.value.region}"
+  ip_cidr_range = "10.8.0.0/24"
   network       = google_compute_network.nat[0].id
-  region        = each.value.region
+#   region        = each.value.region
 }
 
-resource "google_vpc_access_connector" "nat" {
-  for_each = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
-  provider = google-beta
-  project  = google_project.project.project_id
-  name     = "vpcc-${substr(sha256("${each.value.service.name}-${each.value.region}"), 0, 16)}"
-  region   = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].region
+# resource "google_compute_router" "nat" {
+#   for_each = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
+#   provider = google-beta
+#   project  = google_project.project.project_id
+#   name     = "static-ip-router-${each.value.service.name}-${each.value.region}"
+#   network  = google_compute_network.nat[0].name
+#   region   = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].region
+# }
 
-  subnet {
-    name = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].name
-  }
+# resource "google_compute_address" "nat" {
+#   for_each = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
+#   provider = google-beta
+#   project  = google_project.project.project_id
+#   name     = "static-ip-addr-${each.value.service.name}-${each.value.region}"
+#   region   = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].region
+# }
 
-  depends_on = [
-    google_project_service.services
-  ]
-}
+# resource "google_compute_router_nat" "nat" {
+#   for_each = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
+#   provider = google-beta
+#   project  = google_project.project.project_id
+#   name     = "static-nat-${each.value.service.name}-${each.value.region}"
+#   router   = google_compute_router.nat["${each.value.service.name}.${each.value.region}"].name
+#   region   = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].region
 
-resource "google_compute_router" "nat" {
-  for_each = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
-  provider = google-beta
-  project  = google_project.project.project_id
-  name     = "static-ip-router-${each.value.service.name}-${each.value.region}"
-  network  = google_compute_network.nat[0].name
-  region   = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].region
-}
+#   nat_ip_allocate_option = "MANUAL_ONLY"
+#   nat_ips                = [google_compute_address.nat["${each.value.service.name}.${each.value.region}"].self_link]
 
-resource "google_compute_address" "nat" {
-  for_each = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
-  provider = google-beta
-  project  = google_project.project.project_id
-  name     = "static-ip-addr-${each.value.service.name}-${each.value.region}"
-  region   = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].region
-}
-
-resource "google_compute_router_nat" "nat" {
-  for_each = { for entry in (var.gcp_use_nat_for_mongodb_atlas ? local.gcr_services : []): "${entry.service.name}.${entry.region}" => entry }
-  provider = google-beta
-  project  = google_project.project.project_id
-  name     = "static-nat-${each.value.service.name}-${each.value.region}"
-  router   = google_compute_router.nat["${each.value.service.name}.${each.value.region}"].name
-  region   = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].region
-
-  nat_ip_allocate_option = "MANUAL_ONLY"
-  nat_ips                = [google_compute_address.nat["${each.value.service.name}.${each.value.region}"].self_link]
-
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-  subnetwork {
-    name                    = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
-}
+#   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+#   subnetwork {
+#     name                    = google_compute_subnetwork.nat["${each.value.service.name}.${each.value.region}"].id
+#     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+#   }
+# }
