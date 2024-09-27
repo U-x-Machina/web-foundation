0f709cfdd3eb7f0885cb6277b513b5655dbe0636
maciej-uxmachina <maciej@uxmachina.co>
2024-09-27 18:51:30 +0200

    fix: specify min instances for connectors

--- infrastructure/gcp_nat.tf
+++ infrastructure/gcp_nat.tf
@@ -31,6 +31,7 @@ resource "google_vpc_access_connector" "nat" {
   project  = google_project.project.project_id
   name     = "cxn-${each.value}"
   region   = each.value
+  min_instances = 2
   max_instances = 10
 
   subnet {
