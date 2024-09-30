090fd8538b17ec7815d39af3ba5fb5d6416745e4
maciej-uxmachina <maciej@uxmachina.co>
2024-09-30 22:34:37 +0200

    fix: possible to disable basic auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,32 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-  matcher: ["/((?!_next|api|admin).*)"],
-};
+  matcher: ['/((?!_next|api|admin).*)'],
+}
 
 export function middleware(req: NextRequest) {
-  const basicAuthEnabled = process.env.BASIC_AUTH_ENABLED;
+  const basicAuthEnabled = process.env.BASIC_AUTH_ENABLED === 'true'
 
   if (!basicAuthEnabled) {
-    return NextResponse.next();
+    return NextResponse.next()
   }
 
-  const basicAuth = req.headers.get("authorization");
+  const basicAuth = req.headers.get('authorization')
 
   if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1];
-    const [user, pwd] = atob(authValue).split(":");
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
 
-    const validUser = process.env.BASIC_AUTH_USER;
-    const validPassWord = process.env.BASIC_AUTH_PASSWORD;
+    const validUser = process.env.BASIC_AUTH_USER
+    const validPassWord = process.env.BASIC_AUTH_PASSWORD
 
     if (user === validUser && pwd === validPassWord) {
-      return NextResponse.next();
+      return NextResponse.next()
     }
   }
 
   const res = NextResponse.json({ error: 'Authentication required' }, { status: 401 })
-  res.headers.set("WWW-authenticate", 'Basic realm="Secure Area"');
+  res.headers.set('WWW-authenticate', 'Basic realm="Secure Area"')
 
-  return res;
+  return res
 }
