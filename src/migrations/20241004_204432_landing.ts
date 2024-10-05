a85146b2cdd18086b15dc84fc9cfe0b843399d63
maciej-uxmachina <maciej@uxmachina.co>
2024-10-05 14:30:33 +0200

    feat: custom migrations system

--- src/migrations/20241004_204432_landing.ts
+++ src/migrations/20241004_204432_landing.ts
@@ -1,115 +1,157 @@
-import { downloadFile } from '@/utilities/migrations'
+import { Media } from '@/payload-types'
+import { downloadFile, reverseTransaction, transaction } from '@/utilities/migrations'
 import { MigrateDownArgs, MigrateUpArgs } from '@payloadcms/db-mongodb'
 
-/**
- * Creates and uploads a media file for the logo
- * Links the logo in the header and footer;
- * Sets landing page strings;
- */
-export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
-  const logoFilePath = await downloadFile(
-    'https://storage.googleapis.com/uxm-web-foundation-eb51-migrations/20241004_204432_landing/logo-uxmachina-black.svg',
-  )
-
-  const logo = await payload.create({
-    collection: 'media',
-    data: {
-      alt: 'U x Machina logo',
-      caption: {
-        root: {
-          children: [
-            {
-              children: [
-                {
-                  detail: 0,
-                  format: 0,
-                  mode: 'normal',
-                  style: '',
-                  text: 'Black U x Machina logo',
-                  type: 'text',
-                  version: 1,
-                },
-              ],
-              direction: 'ltr',
-              format: '',
-              indent: 0,
-              type: 'paragraph',
-              version: 1,
-              textFormat: 0,
-              textStyle: '',
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'root',
-          version: 1,
-        },
-      },
-    },
-    filePath: logoFilePath,
-  })
-
-  await payload.updateGlobal({
-    slug: 'header',
-    data: {
-      logo: logo.id,
-    },
-  })
-
-  await payload.updateGlobal({
-    slug: 'footer',
-    data: {
-      logo: logo.id,
-    },
-  })
-
-  await payload.updateGlobal({
-    slug: 'landing-page',
-    data: {
-      header: 'Web Foundation',
-      paragraph: 'Demo Project',
-    },
-  })
+interface MigrationContext {
+  logo: Media
 }
 
 /**
- * Deletes the media file with the logo (will remain on GCS though)
- * Unlinks the logo in the header and footer;
- * Unsets landing page strings;
+ * Creates and uploads a Media with the logo
+ * Links the logo in the Header and Hooter
+ * Sets LandingPage strings
  */
-export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
-  const medias = await payload.find({
-    collection: 'media',
-    where: {
-      filename: { equals: 'logo-uxmachina-black.svg' },
-    },
-  })
-  if (medias.totalDocs !== 0) {
-    for (let i = 0; i < medias.docs.length; ++i) {
-      await payload.delete({ collection: 'media', id: medias.docs[0].id })
-    }
-  }
+export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
+  await transaction<MigrationContext>(
+    { payload, req },
+    [
+      // Create logo Media
+      async (ctx: MigrationContext) => {
+        const logoFilePath = await downloadFile(
+          'https://storage.googleapis.com/uxm-web-foundation-eb51-migrations/20241004_204432_landing/logo-uxmachina-black.svg',
+        )
 
-  await payload.updateGlobal({
-    slug: 'header',
-    data: {
-      logo: null,
-    },
-  })
+        ctx.logo = await payload.create({
+          collection: 'media',
+          data: {
+            alt: 'U x Machina logo',
+            caption: {
+              root: {
+                children: [
+                  {
+                    children: [
+                      {
+                        detail: 0,
+                        format: 0,
+                        mode: 'normal',
+                        style: '',
+                        text: 'Black U x Machina logo',
+                        type: 'text',
+                        version: 1,
+                      },
+                    ],
+                    direction: 'ltr',
+                    format: '',
+                    indent: 0,
+                    type: 'paragraph',
+                    version: 1,
+                    textFormat: 0,
+                    textStyle: '',
+                  },
+                ],
+                direction: 'ltr',
+                format: '',
+                indent: 0,
+                type: 'root',
+                version: 1,
+              },
+            },
+          },
+          filePath: logoFilePath,
+        })
+      },
+      // Link logo in the Header
+      async (ctx: MigrationContext) => {
+        await payload.updateGlobal({
+          req,
+          slug: 'header',
+          data: {
+            logo: ctx.logo.id,
+          },
+        })
+      },
+      // Link logo in the Footer
+      async (ctx: MigrationContext) => {
+        await payload.updateGlobal({
+          req,
+          slug: 'footer',
+          data: {
+            logo: ctx.logo.id,
+          },
+        })
+      },
+      // Update LandingPage texts
+      async (ctx: MigrationContext) => {
+        await payload.updateGlobal({
+          req,
+          slug: 'landing-page',
+          data: {
+            header: 'Web Foundation',
+            paragraph: 'Demo Project',
+          },
+        })
+      },
+    ],
+    down,
+  )
+}
 
-  await payload.updateGlobal({
-    slug: 'footer',
-    data: {
-      logo: null,
-    },
-  })
-
-  await payload.updateGlobal({
-    slug: 'landing-page',
-    data: {
-      header: null,
-      paragraph: null,
-    },
-  })
+/**
+ * Deletes the Media with the logo
+ * Unlinks the logo in the Header and Footer
+ * Unsets LandingPage strings
+ */
+export async function down(
+  { payload }: MigrateDownArgs,
+  ctx: MigrationContext | undefined,
+  successfulSteps: number | undefined,
+): Promise<void> {
+  return await reverseTransaction<MigrationContext>(
+    [
+      // Delete created media
+      async (ctx: MigrationContext) => {
+        const medias = await payload.find({
+          collection: 'media',
+          where: {
+            filename: { equals: 'logo-uxmachina-black.svg' },
+          },
+        })
+        if (medias.totalDocs !== 0) {
+          for (let i = 0; i < medias.docs.length; ++i) {
+            await payload.delete({ collection: 'media', id: medias.docs[0].id })
+          }
+        }
+      },
+      // Unlink logo from Header
+      async (ctx: MigrationContext) => {
+        await payload.updateGlobal({
+          slug: 'header',
+          data: {
+            logo: null,
+          },
+        })
+      },
+      // Unlink logo from Footer
+      async (ctx: MigrationContext) => {
+        await payload.updateGlobal({
+          slug: 'footer',
+          data: {
+            logo: null,
+          },
+        })
+      },
+      // Unset LandingPage texts
+      async (ctx: MigrationContext) => {
+        await payload.updateGlobal({
+          slug: 'landing-page',
+          data: {
+            header: null,
+            paragraph: null,
+          },
+        })
+      },
+    ],
+    ctx,
+    successfulSteps,
+  )
 }
--- src/utilities/migrations.ts
+++ src/utilities/migrations.ts
@@ -1,3 +1,4 @@
+import { MigrateDownArgs } from '@payloadcms/db-mongodb'
 import { createWriteStream, existsSync } from 'fs'
 import { mkdir } from 'fs/promises'
 import https from 'https'
@@ -27,4 +28,75 @@ export const downloadFile = async (url: string): Promise<string> => {
       return reject()
     })
   })
+}
+
+/**
+ * Performs a custom transaction by running an array of inidividual steps and by monitoring which one fails.
+ * Upon step failure, it will call the `down` function passing it the failure step for partial reversal.
+ * @param args the original payload migration args
+ * @param steps individual steps to run
+ * @param down a function reversing the above steps
+ */
+export async function transaction<ContextType>(
+  args: MigrateDownArgs,
+  steps: ((ctx: ContextType) => Promise<void>)[],
+  down: (
+    args: MigrateDownArgs,
+    ctx: ContextType | undefined,
+    successfulSteps: number | undefined,
+  ) => Promise<void>,
+) {
+  let successfulSteps = 0
+  let ctx: ContextType = {} as ContextType
+  try {
+    console.log('[transaction] begin')
+    for (let i = 0; i < steps.length; ++i) {
+      console.log(`[transaction] [step ${i + 1}]...`)
+      await steps[i](ctx as any)
+      console.log(`[transaction] [step ${i + 1}] success`)
+      ++successfulSteps
+    }
+    console.log('[transaction] commit')
+  } catch (e) {
+    console.warn('[transaction] error', e)
+    await down(args, ctx, successfulSteps)
+  }
+}
+
+/**
+ * Performs a full or partial reversal of the transaction by running a subset of passed steps
+ * @param steps functions performing individual rollback steps
+ * @param ctx the original payload context
+ * @param successfulSteps index of the last successful step. `undefined` if all steps succeeded (for full rollback)
+ */
+export async function reverseTransaction<ContextType>(
+  steps: ((ctx: ContextType) => Promise<void>)[],
+  ctx: ContextType | undefined,
+  successfulSteps: number | undefined,
+) {
+  // By default (if we run from `payload migrate:down`) failStep will be undefined and we run full rollback
+  let startFrom = steps.length - 1
+  let success = true
+  if (successfulSteps !== undefined) {
+    // Ran inside of a transaction, partial rollback
+    startFrom = successfulSteps - 1
+    console.log('[transaction] rollback')
+  } else {
+    console.log('[transaction] performing full rollback (payload migrate:down)')
+  }
+  try {
+    for (let i = startFrom; i > -1; --i) {
+      console.log(`[transaction] [step ${i + 1}] rollback...`)
+      await steps[i](ctx!)
+      console.log(`[transaction] [step ${i + 1}] rollback success`)
+    }
+    console.log('[transaction] rollback success')
+    success = false
+  } catch (e) {
+    console.error('[transaction] rollback error', e)
+  }
+  if (!success && successfulSteps !== undefined) {
+    console.log('[transaction] throwing error to inform Payload about the rollback')
+    throw new Error('[transaction] rollback')
+  }
 }
