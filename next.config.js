2401f60866dcb28670ffed3eb1c9a2839c409bf1
maciej-uxmachina <maciej@uxmachina.co>
2024-10-13 19:48:36 +0200

    fix: add sentry auth token and fix github ref name

--- next.config.js
+++ next.config.js
@@ -29,6 +29,7 @@ const sentryConfig = {
 
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
@@ -55,7 +56,7 @@ const sentryConfig = {
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  automaticVercelMonitors: false,
 }
 
 export default withSentryConfig(withPayload(nextConfig), sentryConfig)
