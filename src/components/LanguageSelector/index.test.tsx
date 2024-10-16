5391cbdd3275d1a895fb59035aab701290a72abf
maciej-uxmachina <maciej@uxmachina.co>
2024-10-16 21:33:08 +0200

    feat: updated sample unit test

--- src/components/LanguageSelector/index.test.tsx
+++ src/components/LanguageSelector/index.test.tsx
@@ -1,5 +1,37 @@
-import { expect, test } from 'vitest'
+import { render } from '@testing-library/react'
+import { NextIntlClientProvider } from 'next-intl'
+import { expect, test, vi } from 'vitest'
+
+import { LanguageSelector } from '.'
 
 test('LanguageSelector', () => {
-  expect(1).toBe(1)
+  const mocks = vi.hoisted(() => {
+    return {
+      usePathname: vi.fn(),
+      useRouter: vi.fn(),
+    }
+  })
+
+  vi.mock('@/i18n/routing', () => {
+    return {
+      usePathname: mocks.usePathname,
+      useRouter: mocks.useRouter,
+    }
+  })
+
+  const currentLocale = 'en'
+
+  const result = render(
+    <NextIntlClientProvider locale={currentLocale}>
+      <LanguageSelector
+        currentLocale={currentLocale}
+        supportedLocales={[
+          { label: 'English', code: 'en' },
+          { label: 'Polski', code: 'pl' },
+        ]}
+      />
+    </NextIntlClientProvider>,
+  )
+
+  expect(result.getAllByText('English').length).toBe(1)
 })
